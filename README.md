368133b8f47e7c8a3bae880a0cbb25fd3ed3e21f
DharunThota <dharunth@gmail.com>
2024-08-26 21:27:24 +0530

    save model locally

---
+++ README.md
@@ -0,0 +1,46 @@
+
+Model Documentation
+
+Model name: `cardiffnlp/twitter-roberta-base-sentiment-latest`
+This model is a RoBERTa-based model specifically trained for sentiment analysis on tweets.
+
+RoBERTa is a transformers model pretrained on a large corpus of English data in a self-supervised fashion. This means it was pretrained on the raw texts only, with no humans labelling them in any way with an automatic process to generate inputs and labels from those texts.
+
+More precisely, it was pretrained with the Masked language modeling (MLM) objective. Taking a sentence, the model randomly masks 15% of the words in the input then run the entire masked sentence through the model and has to predict the masked words. It allows the model to learn a bidirectional representation of the sentence.
+
+You can use the raw model for masked language modeling, but it's mostly intended to be fine-tuned on a downstream task. Here, we are using it specifically for sentiment analysis.
+
+### Model Description
+
+The `cardiffnlp/twitter-roberta-base-sentiment-latest` model is a sentiment analysis model based on RoBERTa-base architecture, a transformer-based model that enhances BERT by training on more data and with larger batch sizes. This specific model is trained to understand and classify sentiments expressed in tweets.
+
+### Intended Use
+
+This model is intended for sentiment analysis of English tweets. It can classify tweets into three sentiment categories:
+
+- 0: Negative
+- 1: Neutral
+- 2: Positive
+
+### Training Data
+
+The model was trained on approximately 124 million tweets collected from January 2018 to December 2021. The training data includes a wide range of topics and sentiments, ensuring the model is well-rounded for general tweet sentiment analysis tasks
+
+### Evaluation
+
+The model's performance was evaluated using the TweetEval benchmark, which includes various datasets and metrics tailored for tweet sentiment analysis.
+
+TweetEval is a Multi-task Classification Benchmark. It consists of seven heterogenous tasks. The tasks include - irony, hate, offensive, stance, emoji, emotion, and sentiment. All tasks have been unified into the same benchmark, with each dataset presented in the same format and with fixed training, validation and test splits.
+### Limitations
+
+1. **Language Limitation**: The model is designed to work with English tweets only.
+2. **Context Understanding**: The model might struggle with understanding the context or nuances of certain tweets, especially those involving sarcasm, slang, or emerging trends.
+3. **Bias in Training Data**: The training data, being sourced from Twitter, may contain inherent biases present in the platform's user base and content.
+
+
+## How to Run
+Follow the steps below to run the server after cloning/pulling into your local device.  
+You will also need to be running our frontend simultaneously ([mystodon](https://github.com/Vikalp-Social/mystodon)).  
+Default port of the server is set to `3000`.
+
+1) `python api.py` (Ensure all required libraries are installed)
---
+++ api.py
@@ -0,0 +1,569 @@
+from flask import Flask, request, jsonify, Response
+import requests
+from flask_cors import CORS, cross_origin
+import json
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+import torch
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+
+model_name = "cardiffnlp/twitter-roberta-base-sentiment-latest"
+
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+local_dir = "./sentiment"
+
+model.save_pretrained(local_dir)
+tokenizer.save_pretrained(local_dir)
+
+print(f"Model and tokenizer saved to {local_dir}")
+# from transformers import AutoModelForSequenceClassification, AutoTokenizer
+
+# local_dir = "./sentiment"
+
+# model = AutoModelForSequenceClassification.from_pretrained(local_dir)
+# tokenizer = AutoTokenizer.from_pretrained(local_dir)
+
+# print("Model and tokenizer loaded from the local directory")
+
+quantized_model = torch.quantization.quantize_dynamic(
+    model, {torch.nn.Linear}, dtype=torch.qint8
+)
+
+label_to_value = {
+    'negative': -1,
+    'neutral': 0,
+    'positive': 1
+}
+
+def sentiment_anal(toots):
+    results = []
+    for t in toots:
+        if t.get('language', None) == 'en' or (t.get('reblog', {}) and t.get('reblog', {}).get('language', None) == 'en'):
+            results.append(t)
+
+    # print(results)
+    """## Predicting using RoBERTa model"""
+    def predict(model, text):
+        inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True, max_length=512)
+        with torch.no_grad():
+            outputs = model(**inputs)
+        logits = outputs.logits
+        prediction = torch.argmax(logits, dim=-1).item()
+        return prediction
+
+    def get_sentiment(text):
+        prediction = predict(quantized_model, text)
+        label = model.config.id2label[prediction]
+        return label_to_value[label]
+
+    final_list = []
+    for row in results:
+        if row['content'] and get_sentiment(row['content']) == 1:
+            final_list.append(row)
+        elif 'reblog' in row and row['reblog'] and get_sentiment(row['reblog']['content']) == 1:
+            final_list.append(row)
+    return final_list
+
+#register app
+@app.post("/api/v1/register")
+@cross_origin()
+def register_app():
+    try:
+        body = {
+            'client_name': "Vikalp",
+            'redirect_uris': "http://localhost:3001/auth",
+            'scopes': "read write push",
+            'website': "http://localhost:3001"
+        }
+        response = requests.post(f"https://{request.json['instance']}/api/v1/apps", json=body)
+        register_app = json.loads(response.text)
+    except requests.exceptions.ConnectionError as e:
+        return {
+                'error': "Can't Establish a connection to the server",
+                'status': 502,
+                'statusText': "Bad Gateway",
+            }
+    else:
+        if response.status_code >= 400:
+            return ({
+            'error': register_app['error'],
+            'status': response.status_code,
+            'statusText': response.reason,
+        }, response.status_code)
+        else:
+            return register_app
+
+#authenticate user
+@app.post("/api/v1/auth")
+@cross_origin()
+def get_auth_token():
+    #print(request.json)
+    try:
+        body = {
+            'client_id': request.json['id'],
+            'client_secret': request.json['secret'],
+            'redirect_uri': "http://localhost:3001/auth",
+            'grant_type': "authorization_code",
+            'code': request.json['code'],
+            'scope': "read write push",
+        }
+        res1 = requests.post(f"https://{request.json['instance']}/oauth/token", json=body)
+        auth = json.loads(res1.text)
+        headers = {
+            "Authorization": f"Bearer {auth['access_token']}"
+        }
+        res2 = requests.get(f"https://{request.json['instance']}/api/v1/accounts/verify_credentials", headers=headers)
+        verify = json.loads(res2.text)
+    except requests.exceptions.ConnectionError as e:
+        return {
+                'error': "Can't Establish a connection to the server",
+                'status': 502,
+                'statusText': "Bad Gateway",
+            }
+    else:
+        if res1.status_code >= 400:
+            return ({
+            'error': auth['error'],
+            'status': res1.status_code,
+            'statusText': res1.reason,
+        }, res1.status_code)
+        elif res2.status_code >= 400:
+            return ({
+            'error': verify['error'],
+            'status': res2.status_code,
+            'statusText': res2.reason,
+        }, res2.status_code)
+        else:
+            return jsonify({
+                'account': verify,
+                'token': auth['access_token']
+            })
+
+#fetch user account data
+@app.get("/api/v1/accounts/<id>")
+@cross_origin()
+def get_profile(id):
+    try:
+        res1 = requests.get(f"https://{request.args['instance']}/api/v1/accounts/{id}")
+        account = json.loads(res1.text)
+        res2 = requests.get(f"https://{request.args['instance']}/api/v1/accounts/{id}/statuses")
+        statuses = json.loads(res2.text)
+    except requests.exceptions.ConnectionError as e:
+        return {
+                'error': "Can't Establish a connection to the server",
+                'status': 502,
+                'statusText': "Bad Gateway",
+            }
+    else:
+        if res1.status_code >= 400:
+            return ({
+            'error': account['error'],
+            'status': res1.status_code,
+            'statusText': res1.reason,
+        }, res1.status_code)
+        elif res2.status_code >= 400:
+            return ({
+            'error': statuses['error'],
+            'status': res2.status_code,
+            'statusText': res2.reason,
+        }, res2.status_code)
+        else:
+            data = {
+                'account': account,
+                'statuses': {
+                    'count': len(statuses),
+                    'list': statuses
+                }
+            }
+            return data
+
+#edit user profile
+@app.patch("/api/v1/accounts")
+@cross_origin()
+def edit_profile():
+    try:
+        body = {
+            'display_name': request.json['display_name'],
+            'note': request.json['note'],
+        }
+        headers = {
+            'Authorization': f"Bearer {request.json['token']}"
+        }
+        response = requests.patch(f"https://{request.json['instance']}/api/v1/accounts/update_credentials", headers=headers, json=body)
+        edit_profile = json.loads(response.text)
+    except requests.exceptions.ConnectionError as e:
+        return {
+                'error': "Can't Establish a connection to the server",
+                'status': 502,
+                'statusText': "Bad Gateway",
+            }
+    else:
+        if response.status_code >= 400:
+            return ({
+            'error': edit_profile['error'],
+            'status': response.status_code,
+            'statusText': response.reason,
+        }, response.status_code)
+        else:
+            return edit_profile
+    
+#search
+@app.get("/api/v1/search")
+@cross_origin()
+def search():
+    try:
+        headers = {
+            "Authorization": f"Bearer {request.args['token']}"
+        }
+        params = {
+            'q': request.args['q']
+        }
+        response = requests.get(f"https://{request.args['instance']}/api/v2/search", params=params, headers=headers)
+        results = json.loads(response.text)
+    except requests.exceptions.ConnectionError as e:
+        return {
+                'error': "Can't Establish a connection to the server",
+                'status': 502,
+                'statusText': "Bad Gateway",
+            }
+    else:
+        if response.status_code >= 400:
+            return ({
+            'error': results['error'],
+            'status': response.status_code,
+            'statusText': response.reason,
+        }, response.status_code)
+        else:
+            sent = sentiment_anal(results['statuses'])
+            data = {
+                'accounts': results['accounts'],
+                'statuses': sent,
+                'hashtags': results['hashtags']
+            }
+            return data
+
+#follow a user
+@app.post("/api/v1/accounts/<id>/follow")
+@cross_origin()
+def follow_user(id):
+    try:
+        headers = {
+            "Authorization": f"Bearer {request.json['token']}"
+        }
+        response = requests.post(f"https://{request.json['instance']}/api/v1/accounts/{id}/follow", headers=headers)
+        follow = json.loads(response.text)
+    except requests.exceptions.ConnectionError as e:
+        return {
+                'error': "Can't Establish a connection to the server",
+                'status': 502,
+                'statusText': "Bad Gateway",
+            }
+    else:
+        if response.status_code >= 400:
+            return ({
+            'error': follow['error'],
+            'status': response.status_code,
+            'statusText': response.reason,
+        }, response.status_code)
+        else:
+            return follow
+
+#follow a tag
+@app.post("/api/v1/tags/<name>/follow")
+@cross_origin()
+def follow_tag(name):
+    try:
+        headers = {
+            "Authorization": f"Bearer {request.json['token']}"
+        }
+        response = requests.post(f"https://{request.json['instance']}/api/v1/tags/{name}/follow", headers=headers)
+        follow = json.loads(response.text)
+    except requests.exceptions.ConnectionError as e:
+        return {
+                'error': "Can't Establish a connection to the server",
+                'status': 502,
+                'statusText': "Bad Gateway",
+            }
+    else:
+        if response.status_code >= 400:
+            return ({
+            'error': follow['error'],
+            'status': response.status_code,
+            'statusText': response.reason,
+        }, response.status_code)
+        else:
+            return follow
+
+#unfollow a user
+@app.post("/api/v1/accounts/<id>/unfollow")
+@cross_origin()
+def unfollow_user(id):
+    try:
+        headers = {
+            "Authorization": f"Bearer {request.json['token']}"
+        }
+        response = requests.post(f"https://{request.json['instance']}/api/v1/accounts/{id}/unfollow", headers=headers)
+        follow = json.loads(response.text)
+    except requests.exceptions.ConnectionError as e:
+        return {
+                'error': "Can't Establish a connection to the server",
+                'status': 502,
+                'statusText': "Bad Gateway",
+            }
+    else:
+        if response.status_code >= 400:
+            return ({
+            'error': follow['error'],
+            'status': response.status_code,
+            'statusText': response.reason,
+        }, response.status_code)
+        else:
+            return follow
+
+#unfollow a tag
+@app.post("/api/v1/tags/<name>/unfollow")
+@cross_origin()
+def unfollow_tag(name):
+    try:
+        headers = {
+            "Authorization": f"Bearer {request.json['token']}"
+        }
+        response = requests.post(f"https://{request.json['instance']}/api/v1/tags/{name}/unfollow", headers=headers)
+        follow = json.loads(response.text)
+    except requests.exceptions.ConnectionError as e:
+        return {
+                'error': "Can't Establish a connection to the server",
+                'status': 502,
+                'statusText': "Bad Gateway",
+            }
+    else:
+        if response.status_code >= 400:
+            return ({
+            'error': follow['error'],
+            'status': response.status_code,
+            'statusText': response.reason,
+        }, response.status_code)
+        else:
+            return follow
+
+#post a status
+@app.post("/api/v1/statuses")
+@cross_origin()
+def post_status():
+    try:
+        body = {
+            'status': request.json['message'],
+            'media_ids': request.json['media_ids'],
+            'in_reply_to_id': request.json['reply_id'],
+        }
+        headers = {
+            "Authorization": f"Bearer {request.json['token']}"
+        }
+        response = requests.post(f"https://{request.json['instance']}/api/v1/statuses", headers=headers, json=body)
+        post_status = json.loads(response.text)
+    except requests.exceptions.ConnectionError as e:
+        return {
+                'error': "Can't Establish a connection to the server",
+                'status': 502,
+                'statusText': "Bad Gateway",
+            }
+    else:
+        if response.status_code >= 400:
+            return ({
+            'error': post_status['error'],
+            'status': response.status_code,
+            'statusText': response.reason,
+        }, response.status_code)
+        else:
+            return post_status
+
+#edit a status
+@app.put("/api/v1/statuses/<id>")
+@cross_origin()
+def edit_status(id):
+    try:
+        body = {
+            'status': request.json['text']
+        }
+        headers = {
+            "Authorization": f"Bearer {request.json['token']}"
+        }
+        response = requests.put(f"https://{request.json['instance']}/api/v1/statuses/{id}", json=body, headers=headers)
+        edit_status = json.loads(response.text)
+    except requests.exceptions.ConnectionError as e:
+        return {
+                'error': "Can't Establish a connection to the server",
+                'status': 502,
+                'statusText': "Bad Gateway",
+            }
+    else:
+        if response.status_code >= 400:
+            return ({
+            'error': edit_status['error'],
+            'status': response.status_code,
+            'statusText': response.reason,
+        }, response.status_code)
+        else:
+            return edit_status
+
+#favorite or unfavourite a status
+@app.post("/api/v1/statuses/<id>/favourite")
+@cross_origin()
+def favourite(id):
+    try:
+        headers = {
+            "Authorization": f"Bearer {request.json['token']}"
+        }
+        response = requests.post(f"https://{request.json['instance']}/api/v1/statuses/{id}/{request.json['prefix']}favourite", headers=headers)
+        favourite = json.loads(response.text)
+    except requests.exceptions.ConnectionError as e:
+        return {
+                'error': "Can't Establish a connection to the server",
+                'status': 502,
+                'statusText': "Bad Gateway",
+            }
+    else:
+        if response.status_code >= 400:
+            return ({
+            'error': favourite['error'],
+            'status': response.status_code,
+            'statusText': response.reason,
+        }, response.status_code)
+        else:
+            return favourite
+
+#boost or unboost a status
+@app.post("/api/v1/statuses/<id>/boost")
+@cross_origin()
+def boost(id):
+    try:
+        headers = {
+            "Authorization": f"Bearer {request.json['token']}"
+        }
+        response = requests.post(f"https://{request.json['instance']}/api/v1/statuses/{id}/{request.json['prefix']}reblog", headers=headers)
+        boost = json.loads(response.text)
+    except requests.exceptions.ConnectionError as e:
+        return {
+                'error': "Can't Establish a connection to the server",
+                'status': 502,
+                'statusText': "Bad Gateway",
+            }
+    else:
+        if response.status_code >= 400:
+            return ({
+            'error': boost['error'],
+            'status': response.status_code,
+            'statusText': response.reason,
+        }, response.status_code)
+        else:
+            return boost
+
+#fetch a status
+@app.get("/api/v1/statuses/<id>")
+@cross_origin()
+def get_status(id):
+    try:
+        headers = {
+            "Authorization": f"Bearer {request.args['token']}"
+        }
+        res1 = requests.get(f"https://{request.args['instance']}/api/v1/statuses/{id}", headers=headers)
+        status = json.loads(res1.text)
+        res2 = requests.get(f"https://{request.args['instance']}/api/v1/statuses/{id}/context", headers=headers)
+        replies = json.loads(res2.text)
+    except requests.exceptions.ConnectionError as e:
+        return {
+                'error': "Can't Establish a connection to the server",
+                'status': 502,
+                'statusText': "Bad Gateway",
+            }
+    else:
+        if res1.status_code >= 400:
+            return ({
+            'error': status['error'],
+            'status': res1.status_code,
+            'statusText': res1.reason,
+        }, res1.status_code)
+        else:
+            data = {
+                'status': status,
+                'replies': replies['descendants']
+            }
+            return data
+
+#fetch tag timeline
+@app.get("/api/v1/timelines/tag/<name>")
+@cross_origin()
+def get_tag_timeline(name):
+    try:
+        headers = {
+            "Authorization": f"Bearer {request.args['token']}"
+        }
+        params = {
+            'max_id': request.args['max_id']
+        }
+        response = requests.get(f"https://{request.args['instance']}/api/v1/timelines/tag/{name}?limit=30", params=params, headers=headers)
+        tag_timeline = json.loads(response.text)
+    except requests.exceptions.ConnectionError as e:
+        return {
+                'error': "Can't Establish a connection to the server",
+                'status': 502,
+                'statusText': "Bad Gateway",
+            }
+    else:
+        if response.status_code >= 400:
+            return ({
+            'error': tag_timeline['error'],
+            'status': response.status_code,
+            'statusText': response.reason,
+        }, response.status_code)
+        else:
+            sent = sentiment_anal(tag_timeline)
+            data = {
+                'data': sent,
+                'max_id': tag_timeline[-1]['id']
+            }
+            return data
+
+#fetch home timeline
+@app.get("/api/v1/timelines/home")
+@cross_origin()
+def get_timeline():
+    #print(request.args)
+    try:
+        headers = {
+            "Authorization": f"Bearer {request.args['token']}"
+        }
+        params = {
+            'max_id': request.args['max_id']
+        }
+        response = requests.get(f"https://{request.args['instance']}/api/v1/timelines/home?limit=8", headers=headers, params=params)
+        timeline = json.loads(response.text)
+    except requests.exceptions.ConnectionError as e:
+        return {
+                'error': "Can't Establish a connection to the server",
+                'status': 502,
+                'statusText': "Bad Gateway",
+            }
+    else:
+        if response.status_code >= 400:
+            return ({
+            'error': timeline['error'],
+            'status': response.status_code,
+            'statusText': response.reason,
+        }, response.status_code)
+        else:
+            sent = sentiment_anal(timeline)
+            data = {
+                'data': sent,
+                'max_id': timeline[-1]['id']
+            }
+            return data
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3000)
